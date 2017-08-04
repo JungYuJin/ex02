7218ad9ac557a9890128c446f963f0875deadc49
JungYuJin <swan91kr@hanmail.net>
2017-08-04 11:08:51 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -45,5 +45,5 @@ app.use(function(err, req, res, next) {
 
 
 app.listen(3000);
-console.log("server started port:3000 ~~ ");
+console.log("server started port:3000 ~~ !!! ");
 module.exports = app;
